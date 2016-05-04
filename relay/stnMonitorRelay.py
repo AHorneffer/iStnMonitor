836f0a4f6ee3e96994bf5e6f512da89fa2d171d3
Tobia Carozzi <tobia@chalmers.se>
2016-05-04 11:08:05 +0000

    Introduced LOFAR istn status text message conversion to dict.

--- relay/stnMonitorRelay.py
+++ relay/stnMonitorRelay.py
@@ -10,6 +10,7 @@
 RELAYSTNSTAT = False
 SHAMECAST = False
 IPin="192.168.154.201"	#heid eth1
+IPin=""
 IPto="129.16.208.180"	#Simon
 UDP_PORT=6070 #For istnEvn service
 MULTICAST_ADDRESS = 0xe0010104
@@ -21,9 +22,14 @@ logfilename='/home/tobia/lofar/logs/StnStatusLatest.log'
 
 
 import socket
-from struct import pack
+import struct
 
 SHAME_BLOCK_HEAD = '16siiii'
+name = 'LOFAR_SE607'
+size = 0
+version = 0
+timestamp = 0
+flag = 0
 lofar_sbh = struct.pack(SHAME_BLOCK_HEAD, name, size, version, timestamp, flag)
 #struct SHAME_BLOCK {
 #  char name[16];
@@ -33,13 +39,6 @@ lofar_sbh = struct.pack(SHAME_BLOCK_HEAD, name, size, version, timestamp, flag)
 #  int flag;
 #  struct MyData myData;
 #} block;
-
-#LCU_IP="192.168.154.1"		#se607c
-#IPout="129.16.208.120"	#heid eth0
-IPin="192.168.154.201"	#heid eth1
-IPto="129.16.208.180"	#Simon
-#IPto="129.16.208.105"	#blid
-UDP_PORT=6070
 
 
 #Inbound
@@ -63,8 +62,43 @@ sockmout.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
 def stnstat2dict(message):
     """Convert intl station status in the form of an string (as used as message
     between server and cacti client) to a python dict."""
-    pass #Not implemented yet
-
+    #Process status lines
+    status_lns = message.splitlines()
+    #Get iStnStatus version
+    (version_heading, version) = status_lns[0].split(': ')
+    if version == '1.2':
+        (environ_ln, switch_ln, swlevel_ln
+        ) = status_lns[1:4]
+    if version == '2.0':
+       (environ_ln, switch_ln, swlevel_ln, bmctluser_ln
+        ) = status_lns[1:5]
+    #Process environment state line
+    (timestamp, station_keyval, ECvers_keyval, cab3temp_keyval, cab3hum_keyval
+    , heater_keyval, fourty8V_keyval, LCU_keyval, lightning_keyval
+    ) = environ_ln.split(',')
+    
+    #Start creating status dict
+    status = {}
+    keyvalsep = ': '
+    #LOFAR iStnStatus version
+    status['version'] = version
+    #Environment
+    status['time'] =     timestamp
+    (station_key,  status['station']) =  station_keyval.split(keyvalsep)
+    (ECvers_key,   status['ECvers']) =   ECvers_keyval.split(keyvalsep)
+    (cab3temp_key, status['cab3temp']) = cab3temp_keyval.split(keyvalsep)
+    (cab3hum_key,  status['cab3hum']) =  cab3hum_keyval.split(keyvalsep)
+    (heater_key,   status['heater']) =   heater_keyval.split(keyvalsep)
+    (fourty8V_key, status['48V']) =      fourty8V_keyval.split(keyvalsep)
+    (LCU_key,      status['LCU']) =      LCU_keyval.split(keyvalsep)
+    (lightning_key,status['lightning']) =lightning_keyval.split(keyvalsep)
+    #Switch state
+    (switch_key, status['switch']) = switch_ln.split(keyvalsep)
+    #SW level
+    (swlevel_key, status['swlevel']) = swlevel_ln.split(keyvalsep)
+    #Beamctl User
+    (bmctluser_key, status['beamctl']) = bmctluser_ln.split(keyvalsep)
+    return status
 
 def stnstat2shamecast(stnstatdict):
     """Convert intl station status in dict form to a shame cast (OSO event
@@ -73,18 +107,20 @@ def stnstat2shamecast(stnstatdict):
 
 
 while True:
-    message, addr = sockin.recvfrom( 1024 ) # buffer size is 1024 bytes
+    message, addr = sockin.recvfrom( 1*1024 ) # buffer size is 1024 bytes
     
     if isLogging:
         f=open(logfilename, 'w')
         f.write("Latest message:\n")
         f.write(message)
         f.close()
-
+        stnstatdict = stnstat2dict(message)
+        print stnstatdict
+        
     if message[0:4] == "TEST":
         print "Testing:"
         print message
-    if RELAY2CACTICLI:
+    if RELAYSTNSTAT:
         sockout.sendto(message,(IPto,UDP_PORT))
     if SHAMECAST:
     #Multicast
